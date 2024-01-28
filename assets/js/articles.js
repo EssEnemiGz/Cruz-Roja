c3937ff7158af27fa530f4bb59bd31782a7b7fcf
EssEnemiGz <essenemiggz@gmail.com>
2024-01-28 18:44:07 -0400

    "Added responsive and article template to website"

--- assets/js/articles.js
+++ assets/js/articles.js
@@ -57,15 +57,12 @@ addEventOnElements(navTogglers, "click", toggleNav);
  * active header & back top btn when window scroll down to 100px
  */
 
-const header = document.querySelector("[data-header]");
 const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 const activeElementOnScroll = function () {
   if (window.scrollY > 100) {
-    header.classList.add("active");
     backTopBtn.classList.add("active");
   } else {
-    header.classList.remove("active");
     backTopBtn.classList.remove("active");
   }
 }
